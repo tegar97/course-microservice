f7ed0cb814855712e0114a3aaf3289d0059a11bb
Muhammad Tegar Akmal <eategar@gmail.com>
2021-08-28 13:06:40 +0700

    read and delete service media

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -3,7 +3,21 @@ const router = express.Router();
 const base64Img = require("base64-img");
 const isBase64 = require("is-base64");
 const { Media } = require("../models");
+const fs = require("fs");
+router.get("/", async (req, res) => {
+  const media = await Media.findAll();
 
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get("host")}/${m.image} `;
+
+    return m;
+  });
+
+  return res.status(200).json({
+    status: "success",
+    data: mappedMedia,
+  });
+});
 router.post("/", (req, res, next) => {
   const image = req.body.image;
 
@@ -25,7 +39,7 @@ router.post("/", (req, res, next) => {
     const fileName = filepath.split("\\").pop().split("/").pop();
 
     const media = await Media.create({
-      image: `image/${fileName}`,
+      image: `images/${fileName}`,
     });
 
     return res.json({
@@ -34,6 +48,35 @@ router.post("/", (req, res, next) => {
         media: media.id,
         image: `${req.get("host")}/images/${fileName}`,
       },
+    });
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res.status(404).json({
+      status: "error",
+      message: "Media Not Found",
+    });
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    await media.destroy();
+
+    return res.status(200).json({
+      status: "sucess",
+      message: "image deleted",
     });
   });
 });
