c8ceabe5f6b3ff06051ed141e35e7b4f346392f0
Muhammad Tegar Akmal <eategar@gmail.com>
2021-08-29 21:16:18 +0700

    integrated service user to apigateway and add jwt

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const { APP_NAME } = process.env;
+const userHandler = require("./handler/users");
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send(APP_NAME);
-});
+router.post("/register", userHandler.register);
+router.post("/login", userHandler.login);
 
 module.exports = router;
--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userHandler = require("./handler/users/");
 
 router.get("/", userHandler.getUsers);
-router.post("/", userHandler.register);
+router.post("/register", userHandler.register);
 router.post("/login", userHandler.login);
 router.put("/:id", userHandler.update);
 router.get("/:id", userHandler.getUser);
