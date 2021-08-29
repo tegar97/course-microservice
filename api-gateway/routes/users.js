6ddf2b173e9bbb16faafb290db6bbca00d422d3f
Muhammad Tegar Akmal <eategar@gmail.com>
2021-08-29 22:48:00 +0700

    api gateway update profile

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,9 +1,11 @@
 var express = require("express");
+const verifyToken = require("../middleware/verifyToken");
 var router = express.Router();
 const { APP_NAME } = process.env;
 const userHandler = require("./handler/users");
 /* GET users listing. */
 router.post("/register", userHandler.register);
 router.post("/login", userHandler.login);
+router.put("/", verifyToken, userHandler.update);
 
 module.exports = router;
