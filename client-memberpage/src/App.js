23d19fd6868ec1bf107ef37f90fcf19284edc599
Muhammad Tegar Akmal <eategar@gmail.com>
2021-09-05 16:20:52 +0700

    setup react for memberpage

---
+++ client-memberpage/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-red-500">afs</p>
+      <p>tess</p>
+    </div>
+  );
+}
+
+export default App;
